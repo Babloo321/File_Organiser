4c116166e40dd327b5ba586d66dd1a31fc3b5dd2
Babloo Kumar <babloocse888@gmail.com>
2023-06-02 13:17:21 +0530

    completed help command

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
+let helpFun = require("../fileOrganiser/Command/help");
+console.log(helpFun.help());
 switch(command){
     case "tree":
         // call tree function
+        // console.log("tree function call and executed successfully" + path)
         break;
     case "organize":
         // call organize function
+        // console.log("organize function call and executed successfully")
         break;
     case "help":
         // call help function
+        helpFun.help();
+        // console.log("help function call and executed successfully");
         break;
     default:
         console.log("command not recognized:/");
